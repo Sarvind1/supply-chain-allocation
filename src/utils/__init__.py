703559ab44c8e1b7d168abde72ef6bbf233be99f
Sarvind1 <sammyiitb57@gmail.com>
2025-07-14 13:57:01 +0200

    Fix: Add validate_network_integrity to utils exports

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,13 @@
 """Utility functions."""
 
 from .memoization import memoize, CacheStats
-from .csv_loader import load_products, load_nodes, load_edges
+from .csv_loader import load_products, load_nodes, load_edges, validate_network_integrity
 
-__all__ = ["memoize", "CacheStats", "load_products", "load_nodes", "load_edges"]
+__all__ = [
+    "memoize", 
+    "CacheStats", 
+    "load_products", 
+    "load_nodes", 
+    "load_edges",
+    "validate_network_integrity"
+]
